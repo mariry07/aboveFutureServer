c106b08cac26f628cac6769008d0fe33089a0c9c
mary <mariry@163.com>
2019-11-08 16:59:07 +0800

    init

---
+++ db/init.sql
@@ -0,0 +1,3 @@
+#公司消火栓水压，设备类型1101
+insert into tb_sensor_type(id,system_type,device_type,name,fault_name,alarm_name,unit,alarm_level,limit_up,limit_down,pre_limit_up,pre_limit_down) 
+values('110111','11','1101','水压传感器','水压传感故障','水压告警','KPa','1','100','10','90','90');
